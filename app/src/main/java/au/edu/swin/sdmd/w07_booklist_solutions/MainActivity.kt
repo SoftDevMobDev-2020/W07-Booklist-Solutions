e372cd094362b53f6ded04989ff1782c0140091d
N. A. Ronald <nronald@swin.edu.au>
2020-09-28 10:52:53 +1000

    New project.

---
+++ app/src/main/java/au/edu/swin/sdmd/w07_booklist_solutions/MainActivity.kt
@@ -0,0 +1,11 @@
+package au.edu.swin.sdmd.w07_booklist_solutions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
