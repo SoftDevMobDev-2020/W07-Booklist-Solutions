0b8d19aa7a25efeb776545c40668df64bd38e015
N. A. Ronald <nronald@swin.edu.au>
2020-09-28 11:02:53 +1000

    Add Adapter boilerplate and some specific code.

---
+++ app/src/main/java/au/edu/swin/sdmd/w07_booklist_solutions/BooklistAdapter.kt
@@ -0,0 +1,36 @@
+package au.edu.swin.sdmd.w07_booklist_solutions
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class BooklistAdapter(): RecyclerView.Adapter<BooklistAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater
+            .inflate(R.layout.book_layout, parent, false) as View
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = BookStore.count
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = BookStore.books.get(position)
+        holder.bind(item)
+    }
+
+    inner class ViewHolder(val v: View): RecyclerView.ViewHolder(v) {
+
+        fun bind(item: Book) {
+
+        }
+    }
+}
+
+
+
