f7b1c29ff6d7fb1122ddca13165376bc23bd096a
N. A. Ronald <nronald@swin.edu.au>
2020-09-28 11:04:16 +1000

    Set up ViewHolder.

--- app/src/main/java/au/edu/swin/sdmd/w07_booklist_solutions/BooklistAdapter.kt
+++ app/src/main/java/au/edu/swin/sdmd/w07_booklist_solutions/BooklistAdapter.kt
@@ -25,9 +25,16 @@ class BooklistAdapter(): RecyclerView.Adapter<BooklistAdapter.ViewHolder>() {
     }
 
     inner class ViewHolder(val v: View): RecyclerView.ViewHolder(v) {
+        val image = v.findViewById<ImageView>(R.id.cover)
+        val title = v.findViewById<TextView>(R.id.title)
+        val rating = v.findViewById<RatingBar>(R.id.rating)
 
         fun bind(item: Book) {
-
+            title.text = item.title
+            rating.rating = item.rating
+            val imageRes = v.context.resources.getIdentifier("l${item.image}" ,
+                "drawable", v.context.packageName)
+            image.setImageResource(imageRes)
         }
     }
 }
